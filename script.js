11854b301f8e6ced55e1496ca0eae276f3c6ead6
LucianoSant006 <santiagoluciano383@gmail.com>
2025-11-22 15:53:16 -0300

    Add imagens

--- script.js
+++ script.js
@@ -1,3 +1,92 @@
+
+const navbar = document.querySelector("nav");
+window.addEventListener("scroll", () =>
+    navbar.classList.toggle("sticky", window.scrollY > 0)
+);
+
+// Menu Mobile (Hamburguer)
+const menu = document.querySelector(".menu");
+const toggleMenu = () => menu.classList.toggle("active");
+
+document.querySelector(".menu-btn").addEventListener("click", toggleMenu);
+document.querySelector(".close-btn").addEventListener("click", toggleMenu);
+
+document
+    .querySelectorAll(".menu a")
+    .forEach((link) => link.addEventListener("click", toggleMenu));
+
+// Lenis Smooth Scrolling
+const lenis = new Lenis();
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+};
+requestAnimationFrame(raf);
+lenis.on("scroll", ScrollTrigger.update);
+gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
+});
+gsap.ticker.lagSmoothing(0);
+
+
+// Scroll Reveal
+const sr = ScrollReveal({
+    origin: "bottom",
+    distance: "40px",
+    duration: 800,
+    delay: 200,
+    easing: "ease-in-out",
+});
+
+gsap.to("nav", {
+    opacity: 1,
+    duration: 2,
+});
+sr.reveal(".hero-headlines h1");
+sr.reveal(".hero-headlines p", { delay: 500 });
+sr.reveal(".hero-headlines-buttons", { delay: 1000 });
+sr.reveal(".processo-headlines h1");
+sr.reveal(".processo-headlines p", { delay: 500 });
+sr.reveal(".processo-img", { delay: 500 });
+sr.reveal(".pets-headlines");
+sr.reveal(".acao-item", { interval: 200 }); 
+sr.reveal(".sobre-headlines");
+sr.reveal(".img-sobre");
+sr.reveal(".testimunhas h1", { delay: 500 });
+sr.reveal(".testimunhas h6");
+sr.reveal(".testimunhas-item", { delay: 1000, interval: 200 });
+sr.reveal(".footer-brand");
+sr.reveal(".footer-links", { delay: 500, origin: "left" });
+sr.reveal(".footer-doacao", { delay: 500, origin: "bottom" });
+sr.reveal(".copyright", { delay: 600 });
+sr.reveal(".presentes-container", { delay: 300 }); // Animação para nova seção
+
+// GSAP Text Reveal
+const splitTypes = document.querySelectorAll(".reveal-type");
+splitTypes.forEach((char, i) => {
+    const bg = char.dataset.bgColor;
+    const fg = char.dataset.fgColor;
+    const text = new SplitType(char, { type: "chars" }); 
+
+    gsap.fromTo(
+        text.chars,
+        { color: bg, },
+        {
+            color: fg,
+            duration: 0.3,
+            stagger: 0.02,
+            scrollTrigger: {
+                trigger: char,
+                start: "top 80%",
+                end: "top 20%",
+                scrub: true,
+                markers: false,
+                toggleActions: "play play reverse reverse",
+            },
+        }
+    );
+});
+
 document.addEventListener("DOMContentLoaded", () => {
     const container = document.getElementById("carousel-container");
     const prevBtn = document.getElementById("prev-btn");
